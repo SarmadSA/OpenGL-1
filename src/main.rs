36004ed1c4c8c5dffca633d845ce1b1732bde82f
sarmadsa <sarmad@virusoftware.com>
2020-09-17 23:14:32 +0200

    Add missing code from teacher

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 extern crate nalgebra_glm as glm;
-use gl::types::*;
-use std::{
-    mem,
-    ptr,
-    str,
-    os::raw::c_void,
-};
+use std::{ mem, ptr, os::raw::c_void };
 use std::thread;
 use std::sync::{Mutex, Arc, RwLock};
 
 mod shader;
 mod util;
 
-use glutin::event::{Event, WindowEvent, KeyboardInput, ElementState::{Pressed, Released}, VirtualKeyCode::{self, *}};
+use glutin::event::{Event, WindowEvent, DeviceEvent, KeyboardInput, ElementState::{Pressed, Released}, VirtualKeyCode::{self, *}};
 use glutin::event_loop::ControlFlow;
 
 const SCREEN_W: u32 = 800;
 const SCREEN_H: u32 = 600;
 
-// Helper functions to make interacting with OpenGL a little bit prettier. You will need these!
+// == // Helper functions to make interacting with OpenGL a little bit prettier. You *WILL* need these! // == //
 // The names should be pretty self explanatory
 fn byte_size_of_array<T>(val: &[T]) -> isize {
     std::mem::size_of_val(&val[..]) as isize
@@ -39,8 +33,12 @@ fn offset<T>(n: u32) -> *const c_void {
     (n * mem::size_of::<T>() as u32) as *const T as *const c_void
 }
 
-// == // Modify and complete the function below for the first task
+// Get a null pointer (equivalent to an offset of 0)
+// ptr::null()
 
+
+
+// == // Modify and complete the function below for the first task
 //This function sets up a vertex array object (VAO), it takes two arguments that is the data (vertices) and the indices (used to fill the index buffer to tell which vertices should be connected together) and it returns the VAO ID
 unsafe fn setup_vao(vertices: &Vec<f32>, indices: &Vec<u32>, colors: &Vec<f32>) -> u32 { 
     let mut array: u32 = 0; //a pointer to a location where the generated VAO ID can be stored. since we only are allocating a single VAO I created this empty unsigned int
@@ -93,15 +91,23 @@ fn main() {
     let cb = glutin::ContextBuilder::new()
         .with_vsync(true);
     let windowed_context = cb.build_windowed(wb, &el).unwrap();
+    // Uncomment these if you want to use the mouse for controls, but want it to be confined to the screen and/or invisible.
+    // windowed_context.window().set_cursor_grab(true).expect("failed to grab cursor");
+    // windowed_context.window().set_cursor_visible(false);
     
     // Set up a shared vector for keeping track of currently pressed keys
     let arc_pressed_keys = Arc::new(Mutex::new(Vec::<VirtualKeyCode>::with_capacity(10)));
-    // Send a copy of this vector to send to the render thread
+    // Make a reference of this vector to send to the render thread
     let pressed_keys = Arc::clone(&arc_pressed_keys);
+
+    // Set up shared tuple for tracking mouse movement between frames
+    let arc_mouse_delta = Arc::new(Mutex::new((0f32, 0f32)));
+    // Make a reference of this tuple to send to the render thread
+    let mouse_delta = Arc::clone(&arc_mouse_delta);
 
     // Spawn a separate thread for rendering, so event handling doesn't block rendering
     let render_thread = thread::spawn(move || {
-        // Acquire the OpenGL Context and load the function pointers. This has to be done inside of the renderin thread, because
+        // Acquire the OpenGL Context and load the function pointers. This has to be done inside of the rendering thread, because
         // an active OpenGL context cannot safely traverse a thread boundary
         let context = unsafe {
             let c = windowed_context.make_current().unwrap();
@@ -119,48 +125,85 @@ fn main() {
             gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
             gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS);
             gl::DebugMessageCallback(Some(util::debug_callback), ptr::null());
+
+            // Print some diagnostics
+            println!("{}: {}", util::get_gl_string(gl::VENDOR), util::get_gl_string(gl::RENDERER));
+            println!("OpenGL\t: {}", util::get_gl_string(gl::VERSION));
+            println!("GLSL\t: {}", util::get_gl_string(gl::SHADING_LANGUAGE_VERSION));
         }
 
         // == // Set up your VAO here
 
-        //Here I setup the VAO. As mentioned earlier this returns the array ID which I have to use later to draw the primitive
+    //Here I setup the VAO. As mentioned earlier this returns the array ID which I have to use later to draw the primitive
+let value = unsafe {
+    let vertices: Vec<f32> = vec![
+        //Triangle 
+        -0.4, -0.3, 0.6,
+        0.1, -0.3, 0.6,
+       -0.2,  0.3, 0.6,
+       -0.2, -0.3, 0.7,
+        0.4, -0.3, 0.7,
+        0.2,  0.3, 0.7,
+       -0.3, -0.6, 0.8,
+        0.3, -0.6, 0.8,
+        0.0, -0.0, 0.8,
+    ];
+
+    let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let colors: Vec<f32> = vec![
+        0.0, 0.0, 1.0, 0.4,
+        0.0, 0.0, 1.0, 0.4,
+        0.0, 0.0, 1.0, 0.4,
+        1.0, 0.0, 0.0, 0.4,
+        1.0, 0.0, 0.0, 0.4,
+        1.0, 0.0, 0.0, 0.4,
+        0.0, 1.0, 0.0, 0.4,
+        0.0, 1.0, 0.0, 0.4,
+        0.0, 1.0, 0.0, 0.4,
+        ];
+    setup_vao(&vertices, &indices, &colors)    
+};
+
+
+/*     
         let value = unsafe {
-            let vertices: Vec<f32> = vec![
-                //Triangle 
-                -0.4, -0.3, 0.6,
-                0.1, -0.3, 0.6,
-               -0.2,  0.3, 0.6,
-               -0.2, -0.3, 0.7,
-                0.4, -0.3, 0.7,
-                0.2,  0.3, 0.7,
-               -0.3, -0.6, 0.8,
-                0.3, -0.6, 0.8,
-                0.0, -0.0, 0.8,
+    let vertices: Vec<f32> = vec![
+        //Triangle 
+        -0.40, -0.25, 0.6,
+        0.10, -0.25, 0.6,
+       -0.15,  0.30, 0.6,
+       -0.10, -0.25, 0.7,
+        0.40, -0.25, 0.7,
+        0.15,  0.30, 0.7,
+       -0.25, -0.58, 0.8,
+        0.25, -0.58, 0.8,
+        0.00, -0.03, 0.8,
+    ];
+
+        let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let colors: Vec<f32> = vec![
+            0.0, 0.0, 1.0, 0.4,
+            0.0, 0.0, 1.0, 0.4,
+            0.0, 0.0, 1.0, 0.4,
+            1.0, 0.0, 0.0, 0.4,
+            1.0, 0.0, 0.0, 0.4,
+            1.0, 0.0, 0.0, 0.4,
+            0.0, 1.0, 0.0, 0.4,
+            0.0, 1.0, 0.0, 0.4,
+            0.0, 1.0, 0.0, 0.4,
             ];
+        setup_vao(&vertices, &indices, &colors)    
+    };
+    */
 
-            let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-            let colors: Vec<f32> = vec![
-                0.0, 0.0, 1.0, 0.4,
-                0.0, 0.0, 1.0, 0.4,
-                0.0, 0.0, 1.0, 0.4,
-                1.0, 0.0, 0.0, 0.4,
-                1.0, 0.0, 0.0, 0.4,
-                1.0, 0.0, 0.0, 0.4,
-                0.0, 1.0, 0.0, 0.4,
-                0.0, 1.0, 0.0, 0.4,
-                0.0, 1.0, 0.0, 0.4,
-                ];
-            setup_vao(&vertices, &indices, &colors)    
-        };
+    // Basic usage of shader helper
+    // The code below returns a shader object, which contains the field .program_id
+    // The snippet is not enough to do the assignment, and will need to be modified (outside of just using the correct path)
 
-        // Basic usage of shader helper
-        // The code below returns a shader object, which contains the field .program_id
-        // The snippet is not enough to do the assignment, and will need to be modified (outside of just using the correct path)
-
-        //Here I load the shaders, the vertex shader and the fragment shader then they are linked.
-        let shader = unsafe{
-            shader::ShaderBuilder::new().attach_file("./shaders/simple.vert").attach_file("./shaders/simple.frag").link()
-        };
+    //Here I load the shaders, the vertex shader and the fragment shader then they are linked.
+    let shader = unsafe{
+        shader::ShaderBuilder::new().attach_file("./shaders/simple.vert").attach_file("./shaders/simple.frag").link()
+    };
 
         // Used to demonstrate keyboard handling -- feel free to remove
         let mut _arbitrary_number = 0.0;
@@ -190,11 +233,17 @@ fn main() {
                     }
                 }
             }
+            // Handle mouse movement. delta contains the x and y movement of the mouse since last frame in pixels
+            if let Ok(mut delta) = mouse_delta.lock() {
+
+
+
+                *delta = (0.0, 0.0);
+            }
 
             unsafe {
                 gl::ClearColor(0.163, 0.163, 0.163, 1.0);
                 gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-
 
                 // Issue the necessary commands to draw your scene here
 
@@ -212,7 +261,7 @@ fn main() {
                 gl::UniformMatrix4fv(3, 1, gl::FALSE, transformationCombo.as_ptr());
 
                 gl::DrawElements(gl::TRIANGLES, 9, gl::UNSIGNED_INT, ptr::null());
-                
+                 
             }
 
             context.swap_buffers().unwrap();
@@ -272,6 +321,12 @@ fn main() {
                         *control_flow = ControlFlow::Exit;
                     },
                     _ => { }
+                }
+            },
+            Event::DeviceEvent { event: DeviceEvent::MouseMotion { delta }, .. } => {
+                // Accumulate mouse movement
+                if let Ok(mut position) = arc_mouse_delta.lock() {
+                    *position = (position.0 + delta.0 as f32, position.1 + delta.1 as f32);
                 }
             },
             _ => { }
